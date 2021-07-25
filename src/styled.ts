569ab051b2c5fbe22f3c292c8ef575927946e738
Abdul-Rehman-Khan <95abdulrehman@gmail.com>
2021-07-26 01:46:58 +0500

    configure: static tools

---
+++ src/styled.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.div`
+  border-radius: 50%;
+  background-color: #d9dce1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
---
+++ src/teststyles/another/styled.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.div`
+  border-radius: 50%;
+  background-color: #d9dce1;
+  font-family: Hind;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
